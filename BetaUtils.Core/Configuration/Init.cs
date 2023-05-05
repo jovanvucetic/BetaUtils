0aa0c0f533260d96723eb2f013cf1ef5a84589cd
Srki <s.besic@vegait.rs>
2023-05-05 20:38:09 +0200

    Produce warning so build fail

--- BetaUtils.Core/Configuration/Init.cs
+++ BetaUtils.Core/Configuration/Init.cs
@@ -4,6 +4,7 @@ namespace BetaUtils.Core.Configuration
 {
     public class Init
     {
+        [Obsolete]
         public void InitialTestClass()
         {
             throw new ArgumentNullException();
