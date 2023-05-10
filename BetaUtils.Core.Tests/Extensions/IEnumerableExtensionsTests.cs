928c4741293d9dc74a063a23759c894963bf2c6f
Darko Greksa <greksadarko@gmail.com>
2023-05-10 16:23:22 +0200

    Feature/i enumerable extensions (#18)
    * Added custom IEnumerable extensions
    
    * Created Tests project & added unit tests for IEnumerable extensions
    
    * Migrated tests to another project
    
    * Minor fix
    
    * Improved code coverage
    
    * Minor fix
    
    * Renaming and simplifying methods
    
    * Minor fix
    
    * Renaming extensions folder
    
    * Renaming extensions folder pt. 2
    
    ---------
    
    Co-authored-by: s.besic <srdjan.besic@augure.com>

---
+++ BetaUtils.Core.Tests/Extensions/IEnumerableExtensionsTests.cs
@@ -0,0 +1,111 @@
+﻿using BetaUtils.Core.Extensions;
+
+
+
+namespace BetaUtils.Tests.Extensions
+{
+    [TestClass]
+    public class IEnumerableExtensionsTests
+    {
+
+        [TestMethod]
+        public void ForEach_ActionIsNull_ThrowArgumentNullException()
+        {
+            var items = new List<int> { 1, 2, 3 };
+
+            Assert.ThrowsException<ArgumentNullException>(() => IEnumerableExtensions.ForEach(items, null));
+        }
+
+        [TestMethod]
+        public void ForEach_IteratesThroughList_EverythingIsOkay()
+        {
+            var items = new List<int> { 1, 2, 3 };
+
+            var result = IEnumerableExtensions.ForEach(items, item => Console.WriteLine(item));
+
+            CollectionAssert.AreEqual(items, result.ToList());
+        }
+
+        [TestMethod]
+        public void ForEach_WithValidItemsAndAction_PerformsActionOnEachItem()
+        {
+            var items = new List<int> { 1, 2, 3 };
+            var expectedOutput = "1\r\n2\r\n3\r\n";
+
+           var output = new StringWriter();
+            Console.SetOut(output);
+            items.ForEach(item => Console.WriteLine(item));
+
+            Assert.AreEqual(expectedOutput, output.ToString());
+        }
+
+        [TestMethod]
+        public void ToHashSet_ReturnsHashSetWithExpectedItems()
+        {
+            var items = new List<int> { 1, 2, 3 };
+
+            var result = IEnumerableExtensions.ToHashSet(items);
+
+            Assert.AreEqual(3, result.Count);
+        }
+
+        [TestMethod]
+        public void ToHashSet_ReturnsEmptyHashSetWhenInputIsEmpty()
+        {
+            var items = new List<int>();
+
+            var result = IEnumerableExtensions.ToHashSet(items);
+
+            Assert.IsFalse(result.Any());
+        }
+
+        [TestMethod]
+        public void ToObservableCollection_ReturnsObservableCollectionWithExpectedItems()
+        {
+            var items = new List<int> { 1, 2, 3 };
+
+            var result = IEnumerableExtensions.ToObservableCollection(items);
+
+            Assert.AreEqual(3, result.Count);
+        }
+
+        [TestMethod]
+        public void ToObservableCollection_ReturnsEmptyObservableCollectionWhenInputIsEmpty()
+        {
+            var items = new List<int>();
+
+            var result = IEnumerableExtensions.ToObservableCollection(items);
+
+            Assert.IsFalse(result.Any());
+        }
+
+        [TestMethod]
+        public void Batch_ReturnsCorrectNumberOfBatches()
+        {
+            var items = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var result = IEnumerableExtensions.Batch(items, 3);
+
+            Assert.AreEqual(4, result.Count());
+        }
+
+        [TestMethod]
+        public void Batch_ReturnsCorrectItemsInBatches()
+        {
+            var items = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var result = IEnumerableExtensions.Batch(items, 3).ToList();
+
+            Assert.AreEqual(4, result.Count);
+        }
+
+        [TestMethod]
+        public void RemoveNulls_NullsExist_ReturnsNonNullItemsOnly()
+        {
+            var items = new List<int?> { 1, null, 2, null, 3 };
+            var expected = "1,2,3";
+            var result = string.Join(",", items.RemoveNulls());
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ BetaUtils.Core/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace BetaUtils.Core.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Provides an extension method that iterates over the elements
+        /// of an <see cref="IEnumerable{T}"/> and performs 
+        /// the given <paramref name="action"/> on each element.
+        /// </summary>
+        /// <returns>IEnumerable</returns>
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> items, Action<T> action)
+        {
+            if (action is null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            foreach (var item in items)
+            {
+                action(item);
+            }
+
+            return items;
+        }
+
+        /// <summary>
+        /// Converts an IEnumerable to a HashSet
+        /// </summary>
+        /// <returns>HashSet</returns>
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> items)
+        {
+            return new HashSet<T>(items);
+        }
+
+        /// <summary>
+        /// Converts an IEnumerable to an Observable Collection
+        /// </summary>
+        /// <returns>ObservableCollection</returns>
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> items)
+        {
+            return new ObservableCollection<T>(items);
+        }
+
+        /// <summary>
+        /// Takes a collection and splits it into a batch of collections with provided number of values
+        /// </summary>
+        /// <returns>New IEnumerable collections split into collections with provided number of items</returns>
+        public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> items, int numberOfItemsPerBatch)
+        {
+            return items.Select((item, index) => new { item, index })
+                .GroupBy(x => x.index / numberOfItemsPerBatch)
+                .Select(x => x.Select(y => y.item));
+        }
+
+        /// <summary>
+        /// Takes IEnumerable as a source and returns it with all the null values removed
+        /// </summary>
+        /// <param items="source">IEnumerable</param>
+        /// <returns>IEnumerable without null values</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IEnumerable<T> RemoveNulls<T>(this IEnumerable<T> items)
+        {
+            return items.Where(item => item != null);
+        }
+
+    }
+}
