58e920ed62072a27814af6f737caada5139e6c64
Jovan Vucetic <j.vucetic@xximo.com>
2023-07-06 12:47:57 +0200

    Added feature flag manager

---
+++ BetaUtils.Core.Tests/FeatureFlags/FeatureFlagManagerTests.cs
@@ -0,0 +1,47 @@
+﻿using BetaUtils.Core.Configuration;
+using BetaUtils.Core.FeatureFlags;
+using Moq;
+
+namespace BetaUtils.Core.Tests.FeatureFlags
+{
+    [TestClass]
+    public class FeatureFlagManagerTests
+    {
+        [TestMethod]
+        public void Constructor_AllParametersProvided_InstanceCreatedSuccessfully()
+        {
+            var target = new FeatureFlagManagerTestContext().CreateFeatureFlagManager();
+            Assert.IsNotNull(target);
+        }
+
+        [TestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public void IsFeatureEnabled_AnyCase_ValueReturnedFromConfiguration(bool isFeatureEnabled)
+        {
+            var inputValue = "qwerty";
+            var context = new FeatureFlagManagerTestContext();
+
+            context.ConfigurationRepository.Setup(x => x.GetBooleanValueOrDefault($"Features:{inputValue}", false)).Returns(isFeatureEnabled);
+
+            var featureFlagManager = context.CreateFeatureFlagManager();
+
+            var result = featureFlagManager.IsFeatureEnabled(inputValue);
+
+            Assert.AreEqual(isFeatureEnabled, result);
+        }
+
+        private class FeatureFlagManagerTestContext
+        {
+            internal Mock<IConfigurationRepository> ConfigurationRepository;
+
+            public FeatureFlagManagerTestContext()
+            {
+                ConfigurationRepository = new Mock<IConfigurationRepository>();
+            }
+
+            internal FeatureFlagManager CreateFeatureFlagManager()
+               => new FeatureFlagManager(ConfigurationRepository.Object);
+        }
+    }
+}
